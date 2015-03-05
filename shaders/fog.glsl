607e61641f448f1c2f38154ac5e67848dad588bd
Tim Oxley <secoif@gmail.com>
2015-03-05 08:16:22 +0800

    New website.

--- shaders/fog.glsl
+++
@@ -1,8 +0,0 @@
-#pragma glslify: fog = require(glsl-fog/exp)
-
-float fogDensity() {
-  float d = gl_FragCoord.z/gl_FragCoord.w;
-  return fog(d - 40.0, 0.035);
-}
-
-#pragma glslify: export(fogDensity)
